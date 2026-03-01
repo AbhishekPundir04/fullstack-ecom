9bf48901927001a851fe84a5b0729261bf6c7fc7
AbhishekPundir04 <pundirabhishek471@gmail.com>
2026-03-01 15:49:15 +0530

    worked on new setup

---
+++ server/database/db.js
@@ -0,0 +1,21 @@
+import pkg from "pg";
+import dotenv from "dotenv";
+
+const {Client } = pkg;
+
+const database = new Client({
+    user:process.env.DB_USER,
+    host:process.env.DB_HOST,
+    database:process.env.DB_NAME,
+    password:process.env.DB_PASSWORD, 
+    port:process.env.DB_PORT
+})
+
+try{
+    await database.connect();
+    console.log("Database is connected successfully")   
+}catch(error){
+    console.log("Database connection failed", error)
+    process.exit(1);
+}
+export default database;
